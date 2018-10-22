ebadeb2f4d6e05854efc5a1a58dab59748ad680a
KodDaniel <daniel.ahlin1@gmail.com>
2018-10-22 10:55:44 +0200

    DenKlaraVersionenInförTenta

---
+++ EnumSwitchSatsInförTEnta/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumSwitchSatsInförTEnta
+{
+    class Program
+    {
+        static WeaponType weaponType;
+
+        public enum WeaponType
+        {
+            // Our constants 
+            Sword,
+            Staff,
+            Dagger,
+            Mace
+        }
+        static void Main(string[] args)
+        {          
+            // Notera att det är variabeln weaponType som kollas i Switch-satsen...
+            //...och INTE TYPEN WeaponType
+            switch (weaponType)
+            {
+                case WeaponType.Dagger:
+                    Console.WriteLine("We have a Dagger");
+                    break;
+
+                case WeaponType.Mace:
+                    Console.WriteLine("We have a Mace");
+                    break;
+
+                case WeaponType.Staff:
+                    Console.WriteLine("Wwe have a Staff");
+                    break;
+
+                case WeaponType.Sword:
+                    Console.WriteLine("We have a sword");
+                    break;
+
+                default:
+                    Console.WriteLine("We have a UKNOWN weapon.");
+                    break;
+            }
+
+        }
+
+        // An enum is short for "Enumrator".
+        // It can be declared within a class or within a namespace - but not within a function. 
+        // An enum is a way for us to create a datatype which we can limit ourself to specific things.
+        // Like a string is specified to letters...
+        //... and a UInt is specified to positive numbers.
+       
+
+      
+    }
+
+
+}
