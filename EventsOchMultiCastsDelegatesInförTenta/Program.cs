db7b223fb597133289a3cc3dd0dd897f5eedce34
KodDaniel <daniel.ahlin1@gmail.com>
2018-10-25 12:59:59 +0200

    d

--- EventsOchMultiCastsDelegatesInförTenta/Program.cs
+++ EventsOchMultiCastsDelegatesInförTenta/Program.cs
@@ -9,14 +9,70 @@ namespace EventsOchMultiCastsDelegatesInförTenta
     class Program
     {
         static void Main(string[] args)
-        {
+        {          
+            var konto = new Bankkonto();
+            //konto.OnBalanceChange = new BalanceChangeOnHandler(Konto_OnBalanceChange);     
+            konto.OnBalanceChange += new BalanceChangeOnHandler(Konto_OnBalanceChange);
+
         }
-        // a) 14p. Gör en implementation av ett Observer-pattern (Publish-subscribe). Din lösning ska
-        // innehålla en klass som fungerar som Publisher, objekt kan prenumerera på en viss händelse
-        // från objekt av denna Publisher-klass. Din lösning ska använda nyckelordet event samt
-        // innehålla ett testprogram som visar hur en prenumeration görs av händelsen.
-        
-        // b) 4p. Hur påverkas din kod av att nyckelordet event används gentemot om du inte skulle
-        // använda nyckelordet i din kod?
+        static void Konto_OnBalanceChange(object sender, BalanceArgs e)
+        {
+            Console.WriteLine("Saldot ändrat! Nytt saldo: {0} kr",e.Balance);
+        }
+    }
+
+    public class BalanceArgs : EventArgs
+    {
+        public int Balance;
+        public int OldBalance;
+    }
+    //public delegate void BalanceChangeOnHandler(int nyttSaldo);
+    public delegate void BalanceChangeOnHandler(object obj, BalanceArgs balanceArgs);
+
+
+    public class Bankkonto
+    {
+        public event BalanceChangeOnHandler OnBalanceChange;
+
+        //public BalanceChangeOnHandler OnBalanceChange;
+
+        private int _saldo;
+        public int Saldo
+        {
+            get { return _saldo; }
+            set
+            {       
+                var args = new BalanceArgs
+                {
+                    Balance = value,
+                    OldBalance = _saldo
+                };
+                _saldo = value;
+
+                if (OnBalanceChange != null)
+                {
+                    OnBalanceChange(this,args);
+                }
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// a) 14p. Gör en implementation av ett Observer-pattern (Publish-subscribe). Din lösning ska
+// innehålla en klass som fungerar som Publisher, objekt kan prenumerera på en viss händelse
+// från objekt av denna Publisher-klass. Din lösning ska använda nyckelordet event samt
+// innehålla ett testprogram som visar hur en prenumeration görs av händelsen.
+
+// b) 4p. Hur påverkas din kod av att nyckelordet event används gentemot om du inte skulle
+// använda nyckelordet i din kod?
+//svar:
