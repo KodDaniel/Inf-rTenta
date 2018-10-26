8cafa77afdf0e5fbcdbec3a0032ec60d28efcd7e
KodDaniel <daniel.ahlin1@gmail.com>
2018-10-26 15:36:45 +0200

    S

--- EventsOchMultiCastsDelegatesInförTenta3/Program.cs
+++ EventsOchMultiCastsDelegatesInförTenta3/Program.cs
@@ -9,33 +9,103 @@ namespace EventsOchMultiCastsDelegatesInförTenta3
     class Program
     {
         static void Main(string[] args)
-        {
-            // Du skall nu skriva en event-driven chatt-applikation. För att lösa frågan skall du
-            // implementera klasserna ChattServer och ChattKlient enligt nedanstående
-            // specifikationer. Relationerna mellan klasserna är sådana att flera klienter är kopplade
-            // till en server. ChattServer-klassen skall ha en metod som anropas från en klient när
-            // ett nytt meddelande skall skickas ut, metoden skall ha följande signatur:​ ​’void
-            // LäggTillMeddelande(string msg, string användare)’.​
-            
-            // ​Klassen skall även ha ett event med namnet ’NyttMeddelande’ som utlöses när ett nytt meddelande har skickats till
-            // servern. När eventeten utlöses skall det förmedla information om det nya meddelande
-            // samt vilken användare som skrivit det till de som lyssnar på eventet. Eventet skall
-            // deklareras av den fördefinerade delegattypen ’EventHandler’.
-            // ChattKlient-klassen​ ​skall ha två metoder med följande signaturer: ​’void Säg(string
-            // msg)’, ’void Visa(string msg, string användare)’​.​ Visa-metoden skall visa upp ett nytt
-            // meddelandet för användaren och är given nedan. Säg-metoden måste du
-            // implementera själv men du kan anta att den blir anropad när en användare skriver ett
-            // nytt meddelande i GUI:et.
+        {          
+            var instansAvServer = new ChattServer();
+
+            var instansAvChattKlient = new ChattKlient(instansAvServer, "Daniel");
+
+            instansAvChattKlient.Säg("Detta är ett chattmeddelande");
         }
+    }
 
+    public class NyttMeddelandeEventArgs : EventArgs
+    {
+        public string Text { get; set; }
+        public string Användare { get; set; }
 
+        public NyttMeddelandeEventArgs(string text, string användare)
+        {
+            this.Text = text;
+            this.Användare = användare;
+        }
+    }
+        // Publisher-klass
         public class ChattServer
         {
+            // EVENT och DELGATE in same
+            public event EventHandler<NyttMeddelandeEventArgs> NyttMeddelande;
+            
+            public void LäggTillNyttMeddelande(string msg, string användare)
+            {
+                // Om vi varken har ett tomt msg eller en tom användare: 
+                // Gå in i If-satsen
+                if (!string.IsNullOrEmpty(msg) && !string.IsNullOrEmpty(användare))
+                {
+                    // Spara meddelandet och användaren i en instans av...
+                    // NyttMeddelandeEventArgs
+                    var e = new NyttMeddelandeEventArgs(msg,användare);
+                   
+                    //Skickar denna instans till min Event publisher
+                    OnNyttMeddelande(e);
+                }
+                
+            }
 
+            // Event Publisher: tar emot objekt med information deklarerad ovan
+            protected virtual void OnNyttMeddelande(NyttMeddelandeEventArgs e)
+            {
+                // Om vi har subscribers gå in i if-sats
+                if (NyttMeddelande != null)
+                {
+                    //  Skicka Event till Subscribers
+                    NyttMeddelande(this, e);
+                }
+            }
         }
+
+    // Subscriber-Klass
+    public class ChattKlient
+    {
+        // Raden nedan är en instans av min publisher
+        ChattServer Server;
+        string Användare;
+        
+        public ChattKlient(ChattServer server, string användare)
+        {
+            this.Server = server;
+            this.Användare = användare;
+
+            // Upprättar prenumeration 
+            Server.NyttMeddelande+=OnNyttMeddelande;
+            
+        }
+
+        // Event reciver
+        void OnNyttMeddelande(object sender, NyttMeddelandeEventArgs args)
+        {
+            // Anropar metoden visa som har som uppgift att visa på konsol
+            Visa(args.Text, args.Användare);
+        }
+
+        // Visar upp msg för användaren
+        public void Visa(string meddelande, string användare)
+        {
+            Console.WriteLine(användare + ": " + meddelande);
+            Console.ReadLine();
+        }
+
+        //  Vi ANTAR att denna metod anropas när användaren...
+        // skriver ett msg.
+        public void Säg(string meddelande)
+        {
+            Server.LäggTillNyttMeddelande(meddelande, Användare);
+        }
+    }
+
+      
 
 
        
 
-    }
+   
 }
