223719b6c40ff1fef846ec4e01a38c034b779585
KodDaniel <daniel.ahlin1@gmail.com>
2018-12-04 13:04:09 +0100

    d

--- EventsOrdinareOOP2TentaFråga/Program.cs
+++ EventsOrdinareOOP2TentaFråga/Program.cs
@@ -11,16 +11,14 @@ namespace EventsOrdinareOOP2TentaFråga
     {
         static void Main(string[] args)
         {
-            var repo = new CodeRepository(
-             "class Car {public string Brand {get;set;} }"                 
-                );
+            var repo = new CodeRepository("class Car {public string Brand {get;set;} }");
 
-       
-        
             // Upprättar prenumeration
-            repo.Codechanging += Repo_CodeChanging;
-            repo.Codechanging += Repo_CodeChanging2;
+            repo.CodeChanging += Repo_CodeChanging;
+            repo.CodeChanging += Repo_CodeChanging2;
 
+            repo.ProposeChange(6,8,"Katt");
+            
         }
 
         // EventReciver 1; Denna metod prenumerar på mitt CodeChanging-Event och kommer därmed...
@@ -34,6 +32,7 @@ namespace EventsOrdinareOOP2TentaFråga
                 e.RejectOperation();
             }
         }
+
         // EventReciver 2: Denna metod prenumerar på mitt CodeChanging-Event och kommer därmed...
         //...alltid anropas. Men om if-satsen inte är true så jag ju bara gå rakt igenom metoden utan åtgärd.
         static void Repo_CodeChanging2(object sender, CodeChangingEventArgs e)
@@ -53,16 +52,13 @@ namespace EventsOrdinareOOP2TentaFråga
     class RejectableEventArgs : EventArgs
     {
         private bool _isRejected = false;
-         
+
         public bool IsOperationRejected
         {
-            get
-            {
-                return this._isRejected;
-            }
+            get { return this._isRejected; }
         }
 
-       // Om denna metod anropas innebär det att Operation är rejected. 
+        // Om denna metod anropas innebär det att Operation är rejected. 
         public void RejectOperation()
         {
             this._isRejected = true;
@@ -72,10 +68,6 @@ namespace EventsOrdinareOOP2TentaFråga
     class CodeChangingEventArgs : RejectableEventArgs
     {
         public int From { get; private set; }
-
-       
-
-
         public int To { get; private set; }
         public string ReplaceStr { get; private set; }
 
@@ -87,18 +79,19 @@ namespace EventsOrdinareOOP2TentaFråga
         }
     }
 
-    class CodeRepo
+    class CodeRepository
     {
+
+        public EventHandler<CodeChangingEventArgs> CodeChanging;
 
         //Koden kan förvaras i en privat sträng
         private string _code;
 
         //Sätt "code" till ett värde när repot skapas. 
-        public CodeRepo(string firstCode)
+        public CodeRepository(string firstCode)
         {
             _code = firstCode;
 
-            
         }
 
         public void ProposeChange(int from, int to, string replacestr)
@@ -108,16 +101,26 @@ namespace EventsOrdinareOOP2TentaFråga
             string firstHalfOfOldCode = _code.Substring(0, from);
 
             //Plocka ut värdet av code från det att den ska ändras, t.o.m slutet
-            string secondHalfOfOldCode = _code.Substring(to, _code.Length);
+            string secondHalfOfOldCode = _code.Substring(to + 1, (_code.Length - (to + 1)));
 
             //Sätt ihop den första halvan av det som ska behållas av koden, det nya värdet, och den andra halvan av det som ska behållas
             string newCode = firstHalfOfOldCode + replacestr + secondHalfOfOldCode;
 
             //Trigga eventet "Code Changing"
+            OnCodeChanging(new CodeChangingEventArgs(from, to, replacestr));
 
             //Sätt det nya värdet
             _code = newCode;
         }
+
+        protected void OnCodeChanging(CodeChangingEventArgs args)
+        {
+            if (CodeChanging != null)
+            {
+                CodeChanging(this, args);
+            }
+        }
+
     }
 }
 
--- GRASP/Program.cs
+++ GRASP/Program.cs
@@ -10,18 +10,40 @@ namespace GRASP
     {
         static void Main(string[] args)
         {
-            // Här ska man kunna:
-            // Välja spelare
-            // Välja vilken piece (spelpjäs) för respektive spelare  
+          
+            //  Main är mitt UI-lager. Alla övriga klasser är mitt applikationslager. 
             var game = new MonopolyGame();
-
-
+            var james = new Player("James",new Piece("Dog",new Square("Startruta",0)));
+            var linda = new  Player("Linda",new Piece("Horse", new Square("Startruta", 0)));
+            game.Players.Add(james);
+            game.Players.Add(linda);    
+            game.PlayGame();
+          
         }
     }
 
+    // MonopolGame är en klass som är min Controller. En Controller avgör HUR UI-lagret kopplas till applikationslogik-lagret.
+    // Ett vanligt val för Controller är klassen som är en rot-klass/objekt, dvs den klass/objekt som representerar så många andra klasser...
+    ///...i applikationslagret som möjligt. MonopolyGame är en sådan klass.  
     public class MonopolyGame
     {
+        // I föreläsning står: MonopolyGame är ett slags rot-objekt som innesluter de flesta andra domänobjekten.
         public List<Player> Players { get; set; }
+        public List<Piece> Pieces { get;}
+        public Board Board { get;}
+        public Die Die { get;}
+       
+
+        // När vi instansierar ett Game instansierar vi listan Players, annars kan vi ej fylla den från UI. 
+        // När vi instansierar ett Game borde vi också ha ett antal fördefinierade spelpjäser. De är alltid samma och därför fylls de i applikatoinslagret.
+        // Player väljer sedan ett av dessa innan spelstart.
+        public MonopolyGame()
+        {
+            Players = new List<Player>();
+            //Pieces = new List<Piece> {new Piece("Dog"), new Piece("Hat"), new Piece("Cat"),new Piece("Fish")};
+            Board = new Board();
+            Die = new Die();
+        }
 
         private int _roundCnt;
         private int _rounds = 10;
@@ -46,69 +68,111 @@ namespace GRASP
     }
     public class Board
     {
-        // Enligt LRG bör Board känna till alla sina rutor.
-        //Enligt Expert blir därför Board ansvarigt för att känna till:
-        //– den nya rutans läge givet den gamla rutans läge samt
-        //– utfallet från tärningskastet
-        public List<Square> Squares { get; set; }
+        private List<Square> Squares;
 
+        public Board()
+        {
+            Squares = new List<Square>
+            {
+                new Square("Ruta 1",1), new Square("Ruta 2",2), new Square("Ruta 3",3),new Square("Ruta 4",4),new Square("Ruta 5",5),
+                new Square("Ruta 6",6),new Square("Ruta 7",7), new Square("Ruta 8",8), new Square("Ruta 9",9), new Square("Ruta 10", 10),
+                new Square("Ruta 11", 11), new Square("Ruta 12", 12), new Square("Ruta 13",13), new Square("Ruta 14",14)
+            };
+
+
+        }
+                    
+        // Board har kunskap om alla Squares. Därför ska också Board-klassen ha denna metod.
         public List<Square> GetAllSquares()
         {
             return Squares;
         }
 
-        public Square GetSquare(string name)
+        // Board har kunskap om alla Squares. Därför ska också Board-klassen ha denna metod.
+        public Square GetSquare(int oldLocation, int faceValue)
         {
-            throw new NotImplementedException();
+            int newLocation = oldLocation + faceValue;
 
+            return Squares.Where(a => a.Location == newLocation).Single();
+            
         }
-
+        
+            
     }
     public class Square
     {
-        public string Name { get; set; }
+        public string Name { get; }
+        public int Location { get; set; }
+
+        public Square(string name, int location)
+        {
+            this.Name = name;
+            this.Location = location;
+        }
+        
     }
 
     public class Piece
     {
-        // Enligt LRG bör piece (pjäsen) känna till sin ruta (square)
-        // • Enligt Expert skall därför pjäsen (piece) hitta sin nya placering. Information om  den nya placeringen kan den få från sin ägare (player)
-        public string Name { get; set; }
+        public string Name { get;}
+        // och pjäsen sin ruta (square)
         public Square Square { get; set; }
 
+
+        public Piece(string name, Square square)
+        {
+            this.Name = name;
+            this.Square = square;
+        }
+
+        public Square GetLocation(i)
+        {
+
+        }
 
     }
 
     public class Player
     {
-        public string Name { get; set; }
-        // Visibility: Om Player koordinerar stegen krävs objektreferenser till Die, Board och Piece
-        // Enligt LRG bör spelaren (player) känna till sin pjäs(piece)
-        private Die _die = new Die();
-        private Board _board = new Board();
-        private Piece _piece = new Piece();
+        // Enligt LRG bör spelaren (player) känn till sin pjäs(piece) 
+        public string Name { get;}
+        public Piece Piece { get; set; }
 
-
-        public void TakeTurn()
+        public Player(string name, Piece piece)
         {
-            // • Beräkna ett slumptal mellan 2 och 12 (räckvidden för ett tärningskast med två tärningar)
-            // LRG ger anledning att skapa ett  Tärningsobjekt(Die) med ett faceValue attribut.
-            // Att beräkna ett nytt slumptal innebär att ändra information i Die, så • Expert medför att Die skall kunna kasta sig själv(generera nytt slumptal)
-
-            var fv = _die.FaceValue;
-            _die.Roll();
-            fv = _die.FaceValue;
-
-            // Beräkna nästa ruta
-            // Förflytta pjäsen från den gamla rutan till den nya
+            this.Name = name;
+            this.Piece = piece;
         }
 
+        private Die _die = new Die();
+
+        // Denna metod ska innehålla logik för:
+        //  1. Beräkna ett slumptal mellan 2 och 12 (räckvidden för ett tärningskast med två tärningar)
+        // 2. Beräkna nästa ruta
+        // 3. // Förflytta pjäsen från den gamla rutan till den nya
+        public void TakeTurn()
+        {
+            #region Steg 1
+            var fv = _die.FaceValue;
+            _die.Roll();
+            fv = _die.FaceValue; 
+
+
+            #endregion
+        }
 
     }
 
     public class Die
     {
+        
         public int FaceValue { get; private set; }
+
+        public Die()
+        {
+            // Default-värdet för en int är 0. Undviker Null-exception
+            FaceValue = 0;
+        }
 
         // Varför inte låta ”roll” returnera sitt eget värde?
         // Skulle strida mot CQS-principen
